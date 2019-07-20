27939f66463a2038e7aba7ed361470505e5d065d
Rafael <r.montilla.a@gmail.com>
2019-07-19 21:04:10 -0400

    Minor fixes detail view controller

---
+++ CraftsTestProject/New Group/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  CraftsTestProject
+//
+//  Created by Rafael Montilla on 7/19/19.
+//  Copyright © 2019 RMontilla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.white
+    @IBInspectable var bottomColor: UIColor = UIColor.black
+    
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+    
+    override func layoutSubviews() {
+        (layer as! CAGradientLayer).colors = [topColor.cgColor, bottomColor.cgColor]
+    }
+}
+
+extension UIButton {
+    func pulsate() {
+        UIButton.animate(withDuration: 0.2,
+                         animations: {
+                            self.transform = CGAffineTransform(scaleX: 0.975, y: 0.96)
+        },
+                         completion: { finish in
+                            UIButton.animate(withDuration: 0.2, animations: {
+                                self.transform = CGAffineTransform.identity
+                            })
+        })
+    }
+}
---
+++ CraftsTestProject/ViewControllers/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  CraftsTestProject
+//
+//  Created by Rafael Montilla on 7/19/19.
+//  Copyright © 2019 RMontilla. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
